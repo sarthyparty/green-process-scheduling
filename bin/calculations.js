2ca94dad25410f7d40466a186cac49f3e4312bbe
vithulravivarma <vithul@kencorhealth.com>
2023-03-04 22:52:00 -0600

    sun dictionary

--- bin/calculations.js
+++ bin/calculations.js
@@ -1,3 +1,60 @@
+// creating dictionaries with amount of turbines, panels
+
+
+
+let sun = {
+    "Alabama": "",
+    "Alaska": "",
+    "Arizona": "",
+    "Arkansas": "",
+    "California": "",
+    "Connecticut": "",
+    "Delaware": "",
+    "Florida": "",
+    "Georgia": "",
+    "Hawaii": "",
+    "Idaho": "",
+    "Illinois": "",
+    "Indiana": "",
+    "Iowa": "",
+    "Kansas": "",
+    "Kentucky": "",
+    "Louisiana": "",
+    "Maine": "",
+    "Maryland": "",
+    "Massachusetts": "",
+    "Michigan": "",
+    "Minnesota": "",
+    "Mississippi": "",
+    "Missouri": "",
+    "Montana": "",
+    "Nebraska": "",
+    "Nevada": "",
+    "New Hampshire": "",
+    "New Jersey": "",
+    "New Mexico": "",
+    "New York": "",
+    "North Carolina": "",
+    "North Dakota": "",
+    "Ohio": "",
+    "Oklahoma": "",
+    "Oregon": "",
+    "Pennsylvania": "",
+    "Rhode Island": "",
+    "South Carolina": "",
+    "South Dakota": "",
+    "Tennessee": "",
+    "Texas": "",
+    "Utah": "",
+    "Vermont": "",
+    "Virginia": "",
+    "Washington": "",
+    "West Virginia": "",
+    "Wisconsin": "",
+    "Wyoming": ""
+
+}
+
 function calcWindEnergy(speed) {
     return 0.5 * 1.2 * 100 * speed**3 * 0.3 * 7
 }
