5036ac8891c311383282beb608f78b6aa1bb6c8a
Sarthak Agrawal <sarthavader@gmail.com>
2023-03-04 22:07:24 -0600

    setup execute job

--- bin/createJob.js
+++ bin/createJob.js
@@ -1,12 +1,15 @@
+const executeJob = require("./executeJob")
+
 function createJob(bestTime, job) {
     // ...
+    const cron = require("node-cron")
 
     // Schedule tasks to be run on the server.
     cron.schedule('* * * * *', function() {
         executeJob(job)
     });
 
-    console.log(`${bestTime.getHours()}:${bestTime.getMinutes()}`)
+    // console.log(`${bestTime.getHours()}:${bestTime.getMinutes()}`)
 }
 
 module.exports = createJob
